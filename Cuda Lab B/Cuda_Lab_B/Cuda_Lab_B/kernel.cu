31764c74958834b962bd1d841bffbbbd0ef78f9c
CALLUM GRAY <C.GRAY-2019@hull.ac.uk>
2022-02-17 11:51:14 +0000

    ran out of time

---
+++ Cuda Lab B/Cuda_Lab_B/Cuda_Lab_B/kernel.cu
@@ -0,0 +1,109 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void addKernel(int *c, const int *a, const int *b)
+{
+    int i = threadIdx.x;
+    c[i] = a[i] + b[i];
+    printf("Thread ID : %d Block: ID: %d", threadIdx.x, blockIdx.x);
+}
+
+int main()
+{
+    const int size = 5;
+    const int a[size] = { 1, 2, 3, 4, 5 };
+    const int b[size] = { 10, 20, 30, 40, 50 };
+    int c[size] = { 0 };
+
+    int* dev_a = 0;
+    int* dev_b = 0;
+    int* dev_c = 0;
+    
+    // Add vectors in parallel.
+    cudaError_t cudaStatus;
+
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+    // Allocate GPU buffers for three vectors (two input, one output)    .
+    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    // Copy input vectors from host memory to GPU buffers.
+    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) 
+    {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+    cudaStatus = cudaMemcpy(dev_c, c, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess)
+    {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    //addKernel << <1, 5 >> > (dev_c, dev_a, dev_b);
+    //addKernel << <2, 3 >> > (dev_c, dev_a, dev_b);
+    //addKernel << <2, 4 >> > (dev_c, dev_a, dev_b);
+    //addKernel << <2, 5 >> > (dev_c, dev_a, dev_b);
+    //addKernel << <2, 6 >> > (dev_c, dev_a, dev_b);
+    //addKernel << <3, 2 >> > (dev_c, dev_a, dev_b);
+    //addKernel << <3, 3 >> > (dev_c, dev_a, dev_b);
+
+    cudaDeviceSynchronize();
+    cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+
+    cudaFree(dev_c);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+
+    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n", c[0], c[1], c[2], c[3], c[4]);
+
+
+Error:
+    cudaFree(dev_c);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+
+    return 0;
+}
+
